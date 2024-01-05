a1fbbbcf1430054e8cbb4f4fc8a1eddde945cd01
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2024-01-05 11:11:25 -0600

    Generic -> misc to avoid confusion with generics

--- src/test/kotlin/com/replaymod/gradle/remap/misc/TestMiscJavaCode.kt
+++ src/test/kotlin/com/replaymod/gradle/remap/misc/TestMiscJavaCode.kt
@@ -1,24 +1,22 @@
-package com.replaymod.gradle.remap.generic
+package com.replaymod.gradle.remap.misc
 
 import com.replaymod.gradle.remap.util.TestData
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
-class TestGenericJavaCode {
+class TestMiscJavaCode {
     @Test
     fun `remaps methods that have synthetic bridges that change the return type`() {
         TestData.remap("""
-            @org.spongepowered.asm.mixin.Mixin(a.pkg.A.class)
-            abstract class MixinA {
-                private void test() {
+            public class Test {
+                public static void test() {
                     final a.pkg.A value = null;
                     value.aGeneratedSynthetic();
                 }
             }
         """.trimIndent()) shouldBe """
-            @org.spongepowered.asm.mixin.Mixin(b.pkg.B.class)
-            abstract class MixinA {
-                private void test() {
+            public class Test {
+                public static void test() {
                     final b.pkg.B value = null;
                     value.bGeneratedSynthetic();
                 }
